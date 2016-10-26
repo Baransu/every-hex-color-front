fb42ca0f49ecaa6058fa057282c4836444e96ec7
Tomasz Cichocinski <tomaszcichocinski@gmail.com>
2016-10-26 17:30:38 +0200

    basic page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,84 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.min.css';
+import 'materialize-css/dist/css/materialize.min.css';
 import './index.css';
 
-const App = () => {
+// redux store
+const reducer = combineReducers({
+  color: (state = (255*255*255), action) => {
+    console.log(state, action);
+    const number = Math.floor(Math.random() * 255 * 255 * 255);
+    return number;
+  },
+  counter: (state = 0, action) => {
+    return state + 1;
+  }
+});
+
+setInterval(() => {
+  store.dispatch({type: 'NEW_COLOR'})
+}, 1000);
+
+export const store = createStore(reducer, applyMiddleware(thunk));
+
+const AppComponent = ({color, counter}) => {
+
+  const colorStringHex = '#' + color.toString(16);
+
+  const segments = ("0".repeat(8 - counter.toString().length) + counter.toString()).split('');
+
   return (
-    <div>hello world from every color page</div>
+    <div className="continer">
+      <header className="col-xs-12">
+        <h1 className="col-xs-12 head">Every f*cking color</h1>
+      </header>
+      <div className="col-xs-12">
+        <h5 className="col-xs-12 text-center">Global colors left:</h5>
+        <div className="col-xs-12 col-sm-10 col-sm-offset-1 m-b-20 m-t-20 counter-container">
+          <div className="counter">
+            {segments.map((s, i) => (
+               <div key={i} className="segment">{s}</div>
+             ))}
+          </div>
+        </div>
+
+        <div className="col-xs-12 description">
+
+          <form className="col-xs-8 col-xs-offset-2 col-sm-6 col-sm-offset-3">
+            <div className="input-field">
+              <input id="color" type="text" className="" defaultValue={colorStringHex}/>
+            </div>
+          </form>
+
+        </div>
+
+        <div className="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 color m-t-20 m-b-20" style={{backgroundColor: colorStringHex}}></div>
+
+        <div className="col-xs-12">
+          <button className="col-xs-8 col-xs-offset-2 col-sm-6 col-sm-offset-3 btn btn-custom blue-grey lighten-5">Get new color!</button>
+        </div>
+
+        <div className="col-xs-12 m-t-20">
+          tutaj bedzie jakis ladny opis co to jest i do czego sluzy ta ladna bardzo piekna strona
+        </div>
+      </div>
+      {/* add gitusers */}
+      <footer className="col-xs-12 m-t-10 text-center">
+        Copyright © 2016 Cichocinski & Hajto 
+      </footer>
+    </div>
   );
 }
+
+const App = connect(
+  (store) => store,
+  (dispatch) => ({
+
+  })
+)(AppComponent);
 
 export default App;
